a5ce02dd638d5f332d118b23447f3bcf9d94e6f8
Jack-Chamoun <jackchamoun20@gmail.com>
2026-05-12 18:14:12 -0700

    removing waits that were for testing

--- tests/test_go_to_website.py
+++ tests/test_go_to_website.py
@@ -7,7 +7,5 @@ class Test_go_to_website:
         page.goto("https://books.toscrape.com/")
         expect(page.get_by_role("alert")).to_contain_text("Warning! This is a demo website for web scraping purposes. Prices and ratings here were randomly assigned and have no real meaning.")
         expect(page.get_by_role("banner")).to_contain_text("Books to Scrape")
-        page.wait_for_timeout(2000)
         page.locator(Home.MESTERY_BUTTON).click()
-        page.wait_for_timeout(2000)
         expect(page.locator(Mestery.MESTERY_PAGE_TITLE)).to_be_visible()
