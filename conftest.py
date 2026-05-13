59e9b79a8fee76208382bef4968ec69a9dba5631
Jack-Chamoun <jackchamoun20@gmail.com>
2026-05-12 23:36:50 -0700

    modifing the test and adding comments

--- conftest.py
+++ conftest.py
@@ -7,7 +7,7 @@ from datetime import datetime
 @pytest.fixture(scope="function")
 def page():
     with sync_playwright() as p:
-        browser = p.chromium.launch(headless=True)
+        browser = p.chromium.launch(headless=False) # here the headless flag should be from .env file for easier configuration and to make the confiurations in one place
 
         page = browser.new_page()
         page.set_default_timeout(10000)
--- tests/test_go_to_website.py
+++ tests/test_go_to_website.py
@@ -4,8 +4,8 @@ from objectrepository.mestery import Mestery
 
 class Test_go_to_website:
     def test_go_to_website(self, page):
-        page.goto("https://books.toscrape.com/")
-        expect(page.get_by_role("alert")).to_contain_text("Warnings! This is a demo website for web scraping purposes. Prices and ratings here were randomly assigned and have no real meaning.")
+        page.goto("https://books.toscrape.com/") # here the URL should be from .env file for easier configuration and to make the confiurations in one place
+        expect(page.get_by_role("alert")).to_contain_text("Warning! This is a demo website for web scraping purposes. Prices and ratings here were randomly assigned and have no real meaning.")
         expect(page.get_by_role("banner")).to_contain_text("Books to Scrape")
         page.locator(Home.MESTERY_BUTTON).click()
         expect(page.locator(Mestery.MESTERY_PAGE_TITLE)).to_be_visible()
